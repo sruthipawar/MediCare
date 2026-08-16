d8f9136a1c23bd2105ca3d34f02536be852fc0b6
sruthipawar <sruthipawar2006@gmail.com>
2026-08-16 15:22:03 +0530

    Add files via upload

---
+++ client/src/Data/data.js
@@ -0,0 +1,52 @@
+export const userMenu=[
+    {
+    name:"Home",
+    path:"/",
+    icon:"fa-solid fa-house",
+
+},
+{
+    name:"Appointments",
+    path:"/appointments",
+    icon:"fa-solid fa-list",
+},
+{
+    name:"Apply Doctor",
+    path:"/apply-doctor",
+    icon:"fa-solid fa-user-doctor",
+},
+{
+    name:"Contact Us",
+    path:"/contact-us",
+    icon:"fa-solid fa-comments",
+},
+
+]
+//admin menu
+export const adminMenu=[
+    {
+    name:"Home",
+    path:"/",
+    icon:"fa-solid fa-house",
+
+},
+
+{
+    name:" Doctors",
+    path:"/admin/doctors",
+    icon:"fa-solid fa-user-doctor",
+},
+{
+    name:"Users",
+    path:"/admin/users",
+    icon:"fa-solid fa-user",
+},
+{
+    name:"Feedbacks",
+    path:"/user-feedback",
+    icon:"fa-solid fa-comment",
+},
+
+
+]
+
